15210238480348ce1c33c53b8d593f0e3fe753f3
iyahoo <s1200191@gmail.com>
2018-01-11 15:00:23 +0900

    :sparkles: [Add] cljs-http test

--- src-cljs/oxtu/receive/receive.cljs
+++ src-cljs/oxtu/receive/receive.cljs
@@ -1,5 +1,8 @@
 (ns oxtu.cljs.receive
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.core :as reagent :refer [atom]]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
 
 (enable-console-print!)
 
@@ -13,11 +16,17 @@
  [texts]
  (. js/document (getElementById "app")))
 
+(defn test-fn []
+  (go (let [response (<! (http/get "https://api.github.com/users"
+                                   {:with-credentials? false
+                                    :query-params {"since" 135}}))]
+        (prn (:status response))
+        (prn (map :login (:body response))))))
+
+(test-fn)
+
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
-
-
-
